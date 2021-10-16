2d04d737ecf80c67b7a7a9532ad3a58aeb434c9a
Tynen <86012727+TynenW@users.noreply.github.com>
2021-10-16 21:32:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+   const button = question.querySelector(".question-btn");
+
+  button.addEventListener("click", function () {
+    question.classList.toggle("show-text");
+  });
+});
+
+
